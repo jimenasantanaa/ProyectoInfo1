af5e6b1e6a3f7de6153318c075edf156b6bc5d2f
jimen <jimena.belen.santana@estudiantat.upc.edu>
2025-05-05 16:07:47 +0200

    algo

---
+++ version 1/airSpace.py
@@ -0,0 +1,5 @@
+class AirSpace:
+    def __init__(self, nav_points, nav_segments, nav_airports):
+        self.nav_points = nav_points
+        self.nav_segments = nav_segments
+        self.nav_airports = nav_airports
---
+++ version 1/navAirpoint.py
@@ -0,0 +1,5 @@
+class NavAirport:
+    def __init__(self, name, SIDs, STARs):
+        self.name = name
+        self.SIDs = SIDs
+        self.STARs = STARs
