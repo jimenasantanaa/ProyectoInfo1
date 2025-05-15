2142558180355e79f9c5d66081236fe627068371
Anna <anna.diez.rodriguez@estudiantat.upc.edu>
2025-05-15 12:59:01 +0200

    .

--- version 1/airSpace.py
+++ version 1/airSpace.py
@@ -1,23 +1,35 @@
-from navPoint import *
-from navSegment import *
-from NavAirport import *
-from graph import *
+from navPoint import NavPoint, load_navpoints
+from NavSegment import NavSegment, load_navsegment
+from NavAirport import Airport, read_airport
 
 class AirSpace:
-    def __init__(self):
-        self.navpoints = []
-        self.navsegments = []
-        self.navairports = []
+    def __init__(self, navpoints, navsegments, navairports):
+        self.navpoints = navpoints      # Lista de NavPoint
+        self.navsegments = navsegments  # Lista de NavSegment
+        self.navairports = navairports  # Diccionario de Airport
 
-    def cargar_navpoints(self,filename):
+    def cargar_navpoints(self, filename):
         self.navpoints = load_navpoints(filename)
 
-    def cargar_navsegments(self, filename):
+    def cargar_navsegments(self, filename, grafo):
+        self.navsegments = []
+        # Se asume que grafo.navPoint ya contiene los NavPoints
+        self.navsegments = load_navsegment(filename, grafo)
 
-        grafo_temp = Graph()
-        grafo_temp.navPoint = self.navpoints
-        load_navsegment(filename, grafo_temp)
-        self.navsegments = grafo_temp.navSegment
-
-    def cargar_airports(self, filename):
+    def cargar_navairports(self, filename, grafo):
         self.navairports = read_airport(filename)
+        for aeropuerto in self.navairports.values():
+            aeropuerto.relacionar(grafo)
+
+    def cargar_datos(self, prefijo, grafo):
+        navpoints_file = f"{prefijo}_nav.txt"
+        navsegments_file = f"{prefijo}_seg.txt"
+        navairports_file = f"{prefijo}_aer.txt"
+
+        self.cargar_navpoints(navpoints_file)
+        grafo.navPoint = self.navpoints  # Asignar los puntos al grafo
+
+        self.cargar_navsegments(navsegments_file, grafo)
+        self.navsegments = grafo.navSegment  # Actualizar desde el grafo
+
+        self.cargar_navairports(navairports_file, grafo)
