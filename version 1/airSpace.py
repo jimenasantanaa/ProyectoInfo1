7cf39b130cd25af7f4b0621609114a16fc6713dd
Anna <anna.diez.rodriguez@estudiantat.upc.edu>
2025-05-15 12:52:23 +0200

    .

--- version 1/airSpace.py
+++ version 1/airSpace.py
@@ -1,7 +1,23 @@
+from navPoint import *
+from navSegment import *
+from NavAirport import *
+from graph import *
+
 class AirSpace:
-    def __init__(self, NavPoint, NavSegment, NavAirport):
-        self.NavPoint = []
-        self.NavSegment = []
-        self.NavAirport = []
+    def __init__(self):
+        self.navpoints = []
+        self.navsegments = []
+        self.navairports = []
 
+    def cargar_navpoints(self,filename):
+        self.navpoints = load_navpoints(filename)
 
+    def cargar_navsegments(self, filename):
+
+        grafo_temp = Graph()
+        grafo_temp.navPoint = self.navpoints
+        load_navsegment(filename, grafo_temp)
+        self.navsegments = grafo_temp.navSegment
+
+    def cargar_airports(self, filename):
+        self.navairports = read_airport(filename)
