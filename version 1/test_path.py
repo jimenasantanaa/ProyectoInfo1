bc8c732ccca634ab0dca133200957dd96b0633af
jimen <jimena.belen.santana@estudiantat.upc.edu>
2025-05-15 20:55:29 +0200

    algo

--- version 1/test_path.py
+++ version 1/test_path.py
@@ -1,29 +1,31 @@
-from node import *
+from navPoint import NavPoint
 from graph import *
 import matplotlib.pyplot as plt
 from path import *
 
-node_A = Node("A", 1, 2)
-node_B = Node("B", 5, 5)
-node_C = Node("C", 9, 2)
-node_D = Node("D", 13, 7)
+# Crear nodos NavPoint
+node_A = NavPoint("A", 1, 2.0, 1.0)
+node_B = NavPoint("B", 2, 5.0, 5.0)
+node_C = NavPoint("C", 3, 9.0, 2.0)
+node_D = NavPoint("D", 4, 13.0, 7.0)
 
+# Crear Path
 path = Path(node_A)
 
 path.AddNodeToPath(node_B)  # A -> B
 path.AddNodeToPath(node_C)  # B -> C
 path.AddNodeToPath(node_D)  # C -> D
 
-print(path.ContainsNode(node_B))
-print(path.ContainsNode(node_A))
-print(path.ContainsNode(Node("E", 10, 10)))
+print(path.ContainsNode(node_B))  # True
+print(path.ContainsNode(node_A))  # True
+print(path.ContainsNode(NavPoint("E", 5, 10.0, 10.0)))
 
 print("Costo hasta C:", path.CostToNode(node_C))
 
 fig, ax = plt.subplots(figsize=(6, 6))
 for node in [node_A, node_B, node_C, node_D]:
-    ax.scatter(node.coordinate_x, node.coordinate_y, color='green')
-    ax.text(node.coordinate_x, node.coordinate_y, node.name, color='black')
+    ax.scatter(node.longitude, node.latitude, color='green')
+    ax.text(node.longitude, node.latitude, node.name, color='black')
 
 path.PlotPath(ax)
 plt.grid(True)
